90b1404017e5430ba586ef72c0cd90a4096af3ba
Infromatica II <eder.ariza@udea.edu.co>
2020-09-22 13:20:26 -0500

    Ejercicio 3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
+/*
+Ejercicio 3. Escriba un programa que pida dos números A y B e imprima en pantalla el mayor.
+Ej: si se ingresan 7 y 3 se debe imprimir:
+El mayor es 7
+ */
+
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main(){
+
+    int A, B;
+    cout<< "Ingrese el numero A: ";
+    cin>> A;
+    cout<< "Ingrese el numero B: ";
+    cin>> B;
+
+    if(A > B){
+        cout << "El mayor es " << A<< endl;
+    }
+
+    else{
+        cout << "El mayor es " << B<< endl;
+    }
+
+return 0;
 }
